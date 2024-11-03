8bc9ae7c914dedd5d9638abbbac5c4cfd8fbe89f
bonemar8 <bonemar8@gmail.com>
2024-11-03 10:31:32 +0300

    B: count_words func

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
   book_path = "books/frankenstein.txt"
   text = get_text(book_path)
-  print(text)
+  words_count = count_words(text)
+  print(words_count)
 
 
 def get_text(path):
   with open(path) as file:
     return file.read()
 
+
+def count_words(text):
+  words = text.split()
+  return len(words)
 
 main()
